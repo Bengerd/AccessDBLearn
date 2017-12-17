73eda93257215dea69baea2ddc3475a90a62ed1c
ev <ev@itprocomp.ru>
2017-12-17 23:47:59 +0300

    Add Authorithation start window with simple password storage (into the code)

---
+++ AccessDBLearn/Authrithation.xaml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AccessDBLearn
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Authrithation : Window
+    {
+        private string adminPassword = "654321";
+        private string userPassword = "123456";
+
+        public Authrithation()
+        {
+            InitializeComponent();
+        }
+
+        private void enterButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (adminRadioButton.IsChecked == true && passwordTextBox.Text == adminPassword)
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            }
+            else if (userRadioButton.IsChecked == true && passwordTextBox.Text == userPassword)
+            {
+                MainWindow mainWindow = new MainWindow();
+                mainWindow.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show("Неверный пароль", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void exitButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
