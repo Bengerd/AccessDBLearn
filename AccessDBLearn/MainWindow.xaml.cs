399d57d73ba186073c3c2bd74a23498c0de0f5e0
ev <ev@itprocomp.ru>
2017-12-25 15:00:08 +0300

    All features, except filter and refresh datagrid after changes add

--- AccessDBLearn/MainWindow.xaml.cs
+++ AccessDBLearn/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -13,6 +14,10 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Data;
+using System.ComponentModel;
+using System.Data.OleDb;
+//using System.Windows.Forms;
+
 
 namespace AccessDBLearn
 {
@@ -21,9 +26,11 @@ namespace AccessDBLearn
     /// </summary>
     public partial class MainWindow : Window
     {
+        private string connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=\"E:\\ProgrammingSoftware\\Microsoft Visual Studio\\Projects\\AccessDBLearn\\AccessDBLearn\\Baza_Dannykh2.mdb\"";
         private Baza_Dannykh2DataSet baza_Dannykh2DataSet;
         private Dictionary<string, CollectionViewSource> dbViewsCollection = new Dictionary<string, CollectionViewSource>();
-
+        private Dictionary<string, DataGridColumn> dbTableColumn = new Dictionary<string, DataGridColumn>();
+        Baza_Dannykh2DataSetTableAdapters.АбонементTableAdapter adapter;
 
         public MainWindow()
         {
@@ -38,14 +45,18 @@ namespace AccessDBLearn
             {
                 dataGrid.ItemsSource = dbViewsCollection[viewSource].View;
 
-                //for(int i = 0; i < dataGrid.hea; i++)
-                //{ 
-                //    FieldsComboBox.Items.Add(dataGrid.Columns[i].HeaderStringFormat);
-                //}
+                FieldsComboBox.Items.Clear();
+                dbTableColumn.Clear();
+
+                for (int i = 0; i < dataGrid.Columns.Count; i++)
+                {
+                    dbTableColumn.Add(dataGrid.Columns[i].Header.ToString(), dataGrid.Columns[i]);
+                    FieldsComboBox.Items.Add(dataGrid.Columns[i].Header.ToString());
+                }
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.Message);
+                System.Windows.MessageBox.Show(ex.Message);
             }
         }
 
@@ -57,6 +68,7 @@ namespace AccessDBLearn
             // Load data into the table Абонемент. You can modify this code as needed.
             AccessDBLearn.Baza_Dannykh2DataSetTableAdapters.АбонементTableAdapter baza_Dannykh2DataSetАбонементTableAdapter = new AccessDBLearn.Baza_Dannykh2DataSetTableAdapters.АбонементTableAdapter();
             baza_Dannykh2DataSetАбонементTableAdapter.Fill(baza_Dannykh2DataSet.Абонемент);
+            adapter = baza_Dannykh2DataSetАбонементTableAdapter;
             System.Windows.Data.CollectionViewSource абонементViewSource = (System.Windows.Data.CollectionViewSource)(this.FindResource("абонементViewSource"));
             абонементViewSource.View.MoveCurrentToFirst();
             dbViewsCollection.Add("Абонемент", абонементViewSource);
@@ -91,18 +103,119 @@ namespace AccessDBLearn
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.Message);
+                System.Windows.MessageBox.Show(ex.Message);
             }
         }
 
+        #region Selected row and cell visualisation
+
+        private T FindVisualChild<T>(DependencyObject obj) where T : DependencyObject
+        {
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(obj, i);
+                if (child != null && child is T)
+                    return (T)child;
+                else
+                {
+                    T childOfChild = FindVisualChild<T>(child);
+                    if (childOfChild != null)
+                        return childOfChild;
+                }
+            }
+            return null;
+        }
+        private void SetCurrentRow(int rowIndex)
+        {
+            dataGrid.SelectionUnit = DataGridSelectionUnit.FullRow;
+
+            dataGrid.SelectedItems.Clear();
+            /* set the SelectedItem property */
+            object item = dataGrid.Items[rowIndex]; // = Product X
+            dataGrid.SelectedItem = item;
+
+            DataGridRow row = dataGrid.ItemContainerGenerator.ContainerFromIndex(rowIndex) as DataGridRow;
+            if (row == null)
+            {
+                /* bring the data item (Product object) into view
+                 * in case it has been virtualized away */
+                dataGrid.ScrollIntoView(item);
+                row = dataGrid.ItemContainerGenerator.ContainerFromIndex(rowIndex) as DataGridRow;
+            }
+            else
+            {
+                DataGridCell cell = GetCell(dataGrid, row, 0);
+                if (cell != null)
+                    cell.Focus();
+            }
+        }
+        private DataGridCell GetCell(DataGrid dataGrid, DataGridRow rowContainer, int column)
+        {
+            if (rowContainer != null)
+            {
+                DataGridCellsPresenter presenter = FindVisualChild<DataGridCellsPresenter>(rowContainer);
+                if (presenter == null)
+                {
+                    /* if the row has been virtualized away, call its ApplyTemplate() method
+                     * to build its visual tree in order for the DataGridCellsPresenter
+                     * and the DataGridCells to be created */
+                    rowContainer.ApplyTemplate();
+                    presenter = FindVisualChild<DataGridCellsPresenter>(rowContainer);
+                }
+                if (presenter != null)
+                {
+                    DataGridCell cell = presenter.ItemContainerGenerator.ContainerFromIndex(column) as DataGridCell;
+                    if (cell == null)
+                    {
+                        /* bring the column into view
+                         * in case it has been virtualized away */
+                        dataGrid.ScrollIntoView(rowContainer, dataGrid.Columns[column]);
+                        cell = presenter.ItemContainerGenerator.ContainerFromIndex(column) as DataGridCell;
+                    }
+                    return cell;
+                }
+            }
+            return null;
+        }
+
+        private void SelectCellByIndex(int rowIndex, int columnIndex)
+        {
+            dataGrid.SelectionUnit = DataGridSelectionUnit.Cell;
+            dataGrid.SelectedCells.Clear();
+
+            object item = dataGrid.Items[rowIndex]; //=Product X
+            DataGridRow row = dataGrid.ItemContainerGenerator.ContainerFromIndex(rowIndex) as DataGridRow;
+            if (row == null)
+            {
+                dataGrid.ScrollIntoView(item);
+                row = dataGrid.ItemContainerGenerator.ContainerFromIndex(rowIndex) as DataGridRow;
+            }
+            if (row != null)
+            {
+                DataGridCell cell = GetCell(dataGrid, row, columnIndex);
+                if (cell != null)
+                {
+                    DataGridCellInfo dataGridCellInfo = new DataGridCellInfo(cell);
+                    dataGrid.SelectedCells.Add(dataGridCellInfo);
+                    cell.Focus();
+                }
+            }
+        }
+
+        #endregion
+
+
         private void firstRowButton_Click(object sender, RoutedEventArgs e)
         {
-            dataGrid.SelectedIndex = 0;
+            SetCurrentRow(0);
+            // dataGrid.SelectedIndex = 0;
+
         }
 
         private void lastRowButton_Click(object sender, RoutedEventArgs e)
         {
-            dataGrid.SelectedIndex = dataGrid.Items.Count - 2;
+            SetCurrentRow(dataGrid.Items.Count - 2);
+            //dataGrid.SelectedIndex = dataGrid.Items.Count - 2;
         }
 
         private void customRowButton_Click(object sender, RoutedEventArgs e)
@@ -118,12 +231,19 @@ namespace AccessDBLearn
             else
                 nextSelectItem = dataGrid.SelectedIndex + selectItemModifer;
 
-            dataGrid.SelectedIndex = nextSelectItem;
+            SetCurrentRow(nextSelectItem);
+            //dataGrid.SelectedIndex = nextSelectItem;
         }
 
         private void FieldsComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
+            ICollectionView view = CollectionViewSource.GetDefaultView(dataGrid.ItemsSource);
 
+            view.SortDescriptions.Clear();
+            //create a new sort order for the sorting that is done lastly
+            view.SortDescriptions.Add(new SortDescription(FieldsComboBox.SelectedValue.ToString(), ListSortDirection.Ascending));
+            //refresh the view which in turn refresh the grid
+            view.Refresh();
         }
 
         private void exitButton1_Click(object sender, RoutedEventArgs e)
@@ -134,7 +254,75 @@ namespace AccessDBLearn
         private void sqlButton_Click(object sender, RoutedEventArgs e)
         {
             QueryEditor queryEditor = new QueryEditor();
-            queryEditor.Show();
+            queryEditor.ShowDialog();
+        }
+
+        private void changeButton_Click(object sender, RoutedEventArgs e)
+        {
+            DataRowView row = (DataRowView)dataGrid.Items.CurrentItem;
+
+            int itemID = Convert.ToInt32(row[0]);
+            SubscriptionsAdd subscriptionsWindow = new SubscriptionsAdd(false, row);
+            subscriptionsWindow.ShowDialog();
+        }
+
+        private void deleteButton_Click(object sender, RoutedEventArgs e)
+        {
+            DataRowView row = (DataRowView)dataGrid.Items.CurrentItem;
+
+            int itemID = Convert.ToInt32(row[0]);
+            string sql = string.Format(@"
+delete from Абонемент 
+where
+    [код Абонемента] = {0}
+", row[0].ToString()
+);
+            OleDbConnection connection = new OleDbConnection(connectionString);
+
+            using (OleDbCommand com = new OleDbCommand(sql, connection))
+            {
+                connection.Open();
+
+                com.ExecuteNonQuery();
+            }
+        }
+
+        private void addButton_Click(object sender, RoutedEventArgs e)
+        {
+            SubscriptionsAdd subscriptionsWindow = new SubscriptionsAdd(true);
+            subscriptionsWindow.ShowDialog();
+        }
+
+        private void findTextBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            string columnName = FieldsComboBox.SelectedValue.ToString();
+            string findItem = findTextBox.Text;
+            int rowIndex = -1;
+
+            for (int i = 0 ; i < dataGrid.Items.Count; i++)
+            {
+                DataRowView row = (DataRowView)dataGrid.Items[i];
+
+                if (row[columnName].ToString() == findItem)
+                {
+                    rowIndex = i;
+                    break;
+                }
+            }
+            
+            //DataRowView row = (DataRowView)dataGrid.Items[0];
+            if (rowIndex == -1)
+            {
+                MessageBox.Show("Совпадений не найдено");
+            }
+            else
+                SetCurrentRow(rowIndex);
+
+        }
+
+        private void updateButton_Click(object sender, RoutedEventArgs e)
+        {
+                
         }
     }
 }
--- AccessDBLearn/SubscriptionsAdd.xaml.cs
+++ AccessDBLearn/SubscriptionsAdd.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Data;
+using System.Data.OleDb;
 
 namespace AccessDBLearn
 {
@@ -19,9 +21,107 @@ namespace AccessDBLearn
     /// </summary>
     public partial class SubscriptionsAdd : Window
     {
-        public SubscriptionsAdd()
+        private string connectionString = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=\"E:\\ProgrammingSoftware\\Microsoft Visual Studio\\Projects\\AccessDBLearn\\AccessDBLearn\\Baza_Dannykh2.mdb\"";
+        private DataRowView row;
+        private bool isAddNew = true;
+
+        Baza_Dannykh2DataSet baza_Dannykh2DataSet;
+        AccessDBLearn.Baza_Dannykh2DataSetTableAdapters.АбонементTableAdapter baza_Dannykh2DataSetАбонементTableAdapter;
+
+
+        public SubscriptionsAdd(bool isAddNew, DataRowView row = null)
         {
             InitializeComponent();
+
+            this.row = row;
+            this.isAddNew = isAddNew;
+        }
+
+        private void acceptButton_Click(object sender, RoutedEventArgs e)
+        {
+            string sql = "";
+
+            if (isAddNew)
+            {
+                sql = string.Format(@"
+insert into Абонемент
+(
+    [фамилия клиента]
+    ,[возраст клиента]
+    ,[рост клиента]
+    ,[вес клиента]
+    ,[тренер]
+)
+values
+(
+    '{0}'
+    ,{1}
+    ,{2}
+    ,{3}
+    ,'{4}'
+)
+", lastNameTextBox.Text
+   ,ageTextBox.Text
+   ,heightTextBox.Text
+   ,weightTextBox.Text
+   ,trainerTextBox.Text
+);
+            }
+            else
+            {
+                sql = string.Format(@"
+update 
+    Абонемент 
+set
+    [фамилия клиента] = '{0}'
+    ,[возраст клиента] = {1}
+    ,[рост клиента] = {2}
+    ,[вес клиента] = {3}
+    ,[тренер] = '{4}'
+where
+    [код Абонемента] = {5}
+"  , lastNameTextBox.Text
+   , ageTextBox.Text
+   , heightTextBox.Text
+   , weightTextBox.Text
+   , trainerTextBox.Text
+   , row[0].ToString()
+);
+            }
+
+            OleDbConnection connection = new OleDbConnection(connectionString);
+
+            using (OleDbCommand com = new OleDbCommand(sql, connection))
+            {
+                connection.Open();
+
+                com.ExecuteNonQuery();
+            }
+
+            this.Close();
+        }
+
+        private void cancelButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            /*
+            baza_Dannykh2DataSet = ((AccessDBLearn.Baza_Dannykh2DataSet)(this.FindResource("baza_Dannykh2DataSet")));
+            baza_Dannykh2DataSetАбонементTableAdapter = new AccessDBLearn.Baza_Dannykh2DataSetTableAdapters.АбонементTableAdapter();
+            baza_Dannykh2DataSetАбонементTableAdapter.Fill(baza_Dannykh2DataSet.Абонемент);
+            */
+            if (!isAddNew)
+            {
+                codeTextBox.Text = row[0].ToString();
+                lastNameTextBox.Text = row[1].ToString();
+                ageTextBox.Text = row[2].ToString();
+                heightTextBox.Text = row[3].ToString();
+                weightTextBox.Text = row[4].ToString();
+                trainerTextBox.Text = row[5].ToString();
+            }
         }
     }
 }
